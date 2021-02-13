a9310c91e40c7a0b7cc0bcfb69c2f737bb9b458e
turkaytunc <turkaytunc00@gmail.com>
2021-02-13 13:54:39 +0300

    add: tuples

--- src/app.ts
+++ src/app.ts
@@ -34,10 +34,30 @@ class ProjectInput {
     this.attach();
   }
 
+  private gatherUserInput(): [string, string, number] | void | null {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredPeople.trim().length === 0 ||
+      enteredDescription.trim().length === 0
+    ) {
+      return null;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+
   @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput;
+      console.log(title, description, people);
+    }
   }
 
   private configure() {
